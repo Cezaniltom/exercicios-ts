6ec3e5999ec2afd089057e30e83b93b5a3b663df
cezaniltom <cezaniltom@outlook.com>
2024-09-13 22:01:23 -0300

    ajustar amanha

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+// type Aluno = {
+//     nome: string;
+//     altura: number;
+//     dataNasc: Date;
+//     modulosCursados: Array<string>; // string[] ou Array<string>
+//     ativo?: boolean;
+//     estudar?: () => void;
+//     parabenizar?: () => void;
+// };
+class Animal {
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
